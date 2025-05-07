c7d42ff2ee6e43a40184fb8f68e8127960cd83aa
chaijewon <vcandjava@nate.com>
2025-05-07 15:59:40 +0900

    1111

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RefView/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+// import MsgView from './components/MsgView.vue';
+// import DirectiveView from './components/DirectiveView.vue';
+import RefView from './components/RefView.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RefView
   }
 }
 </script>
